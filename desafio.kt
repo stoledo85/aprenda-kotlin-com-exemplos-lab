703f7b4253d02829973a0e5eb087467d739be049
Sander Toledo <sanderaugusto@gmail.com>
2024-07-04 21:01:17 -0300

    resolucao projeto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,56 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(var nome:String,var idade:Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60,var desc:String)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel:Nivel ,var conteudos: MutableList<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        print("Aluno ${usuario.nome} está matriculado na formação $nome.")
+    }
+    fun criarConteudo(conteudoedu :ConteudoEducacional){
+        conteudos.add(conteudoedu)
+        print("Conteúdo criado.")
+    }
+    fun removerConteudo(conteudoedu :ConteudoEducacional){
+        conteudos.remove(conteudoedu)
+         println("Conteúdo '${conteudoedu.nome}' removido ")
+    }
+    fun exibir() {
+        println("Formação: $nome")
+        println("Nível: $nivel")
+        println("Conteúdos:")
+        for (conteudo in conteudos) {
+            println(" - ${conteudo.nome} (${conteudo.duracao} minutos) - ${conteudo.desc})")
+        }
+        println("Inscritos:")
+        for (inscrito in inscritos) {
+            println(" - ${inscrito.nome})")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario(nome="Carlos", idade=25)
+    val usuario2 = Usuario(nome="Lucas", idade=18)
+    val usuario3 = Usuario(nome="José", idade=36)
+
+    val cont1 = ConteudoEducacional(nome="Programação OO em Kotlin", desc="Conteudo de Introdutorio a POO com Kotlin",duracao=60)
+    val cont2 = ConteudoEducacional(nome="Programação OO em Kotlin", desc="Logica de programação com Kotlin",duracao=36)
+    
+    val formacaoKotlin = Formacao(nome="Formação Kotlin", nivel=Nivel.BASICO)
+    formacaoKotlin.criarConteudo(cont1)
+    formacaoKotlin.criarConteudo(cont2)
+
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+    formacaoKotlin.matricular(usuario3)
+
+    formacaoKotlin.exibir()
 }
